bddd799489e4a143e9b619a5411495bbc90eb57d
deelay <gdeslarzes03@protonmail.com>
2024-04-26 21:10:57 +0200

    Deleted runner scripts & finished kernel.img

--- run-debug.sh
+++
@@ -1,4 +0,0 @@
-cargo build
-arm-none-eabi-objcopy -O binary ./target/armv7a-none-eabi/debug/blinky-os ./kernel.img
-rm -r ./target
-qemu-system-aarch64 -machine raspi3b -semihosting-config enable=on,target=native -kernel ./kernel.img
--- run-release.sh
+++
@@ -1,4 +0,0 @@
-cargo build --release
-arm-none-eabi-objcopy -O binary ./target/armv7a-none-eabi/release/blinky-os ./kernel.img
-rm -r ./target
-qemu-system-aarch64 -machine raspi3b -semihosting-config enable=on,target=native -kernel ./kernel.img
